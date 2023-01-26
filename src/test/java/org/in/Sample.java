392fbbedc0d6b47bd359ad5925d7c6d3d58f92f8
SUDHAGAR98 <sudhagar20398@gmail.com>
2023-01-26 21:20:21 +0530

    firsttime master

---
+++ src/test/java/org/in/Sample.java
@@ -0,0 +1,22 @@
+package org.in;
+
+public class Sample {
+	
+	private void method1() {
+		System.out.println("method1");
+
+	}
+	private void method2() {
+
+		System.out.println("medhod2");
+	}
+	
+	
+	
+	
+	
+
+	
+	
+	
+}
